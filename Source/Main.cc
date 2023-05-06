efd97958614f4294f2a122d1d12c4a714318f068
Sergei Belov <sergei-belov@outlook.com>
2023-05-06 21:36:02 +0300

    Configured building and linking of IMGU and added demo window.

--- Source/Main.cc
+++ Source/Main.cc
@@ -1,4 +1,8 @@
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_opengl3.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
 
 int main(int argc, char* argv[])
 {
@@ -7,18 +11,42 @@ int main(int argc, char* argv[])
 	auto* window = SDL_CreateWindow("Sample", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
 	auto* context = SDL_GL_CreateContext(window);
 
+	ImGui::CreateContext();
+
+	ImGui_ImplSDL2_InitForOpenGL(window, context);
+	ImGui_ImplOpenGL3_Init("#version 150");
+
 	bool exit = false;
 	while (!exit)
 	{
 		SDL_Event sdlEvent;
 		while (SDL_PollEvent(&sdlEvent))
 		{
+			ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
+
 			if (sdlEvent.type == SDL_QUIT)
 			{
 				exit = true;
 			}
 		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::ShowDemoWindow();
+
+		ImGui::Render();
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		SDL_GL_SwapWindow(window);
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
 
 	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
