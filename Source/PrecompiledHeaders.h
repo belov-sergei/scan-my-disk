191c3af9969ff6a50c62907c95ec9435c497920f
Sergei Belov <sergei-belov@outlook.com>
2023-05-06 21:08:06 +0300

    Added precompiled headers.

---
+++ Source/PrecompiledHeaders.h
@@ -0,0 +1,138 @@
+﻿#pragma once
+
+// Concepts library.
+#include <concepts>
+
+// Coroutines library.
+#include <coroutine>
+
+// Utilities library.
+#include <any>
+#include <bitset>
+#include <chrono>
+#include <compare>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <expected>
+#include <functional>
+#include <initializer_list>
+#include <optional>
+#include <source_location>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <variant>
+#include <version>
+
+// Dynamic memory management.
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <scoped_allocator>
+
+// Numeric limits.
+#include <cfloat>
+#include <cinttypes>
+#include <climits>
+#include <cstdint>
+#include <limits>
+
+// Error handling.
+#include <cassert>
+#include <cerrno>
+#include <exception>
+#include <stacktrace>
+#include <stdexcept>
+#include <system_error>
+
+// Strings library.
+#include <cctype>
+#include <charconv>
+#include <cstring>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+#include <format>
+#include <string>
+#include <string_view>
+
+// Containers library.
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <span>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// Iterators library.
+#include <iterator>
+
+// Ranges library.
+#include <ranges>
+
+// Algorithms library.
+#include <algorithm>
+#include <execution>
+
+// Numerics library.
+#include <bit>
+#include <cfenv>
+#include <cmath>
+#include <complex>
+#include <numbers>
+#include <numeric>
+#include <random>
+#include <ratio>
+#include <valarray>
+
+// Localization library.
+#include <clocale>
+#include <codecvt>
+#include <locale>
+
+// Input/output library.
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <spanstream>
+#include <sstream>
+#include <streambuf>
+#include <strstream>
+#include <syncstream>
+
+// Filesystem library.
+#include <filesystem>
+
+// Regular Expressions library.
+#include <regex>
+
+// Atomic Operations library.
+#include <atomic>
+
+// Thread support library.
+#include <barrier>
+#include <condition_variable>
+#include <future>
+#include <latch>
+#include <mutex>
+#include <semaphore>
+#include <shared_mutex>
+#include <stop_token>
+#include <thread>
--- Source/Test.h
+++ Source/Test.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 inline void PrintMessage()
 {
 	std::cout << "Empty CMake project." << std::endl;
