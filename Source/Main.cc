d2647c584f676ac8957f63c99a5bb650f8cbf728
Sergei Belov <sergei-belov@outlook.com>
2023-10-06 23:13:55 +0300

    Implemented pie chart visualization with interactive mouse interaction.

--- Source/Main.cc
+++ Source/Main.cc
@@ -4,11 +4,13 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
-int main(int argc, char* argv[])
-{
+#include <Chart.h>
+#include <Filesystem.h>
+
+int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	auto* window = SDL_CreateWindow("Sample", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+	auto* window = SDL_CreateWindow("Sample", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768, SDL_WINDOW_OPENGL);
 	auto* context = SDL_GL_CreateContext(window);
 
 	ImGui::CreateContext();
@@ -16,16 +18,38 @@ int main(int argc, char* argv[])
 	ImGui_ImplSDL2_InitForOpenGL(window, context);
 	ImGui_ImplOpenGL3_Init("#version 150");
 
+	const auto filesystemTree = Filesystem::BuildTree("C://Projects//Disk Usage");
+
+	std::vector<std::tuple<float, float, float, float>> sliceDrawData;
+
+	std::vector<float> depthStartAngle(1);
+
+	size_t size = filesystemTree->size;
+	filesystemTree.depthTraversal([&](const Tree::Node<Filesystem::Entry>& entry) {
+		if (depthStartAngle.size() < entry->depth + 2) {
+			depthStartAngle.emplace_back();
+		}
+
+		float& start = depthStartAngle[entry->depth];
+		depthStartAngle[entry->depth + 1] = start;
+
+		const float normalized = entry->size / (float)size;
+		if (normalized > 0.01) {
+			sliceDrawData.emplace_back(40 * (entry->depth + 1), start, start + normalized, std::max(0.0f, 0.33f - normalized * 0.66f));
+		}
+
+		start += normalized;
+
+		return false;
+	});
+
 	bool exit = false;
-	while (!exit)
-	{
+	while (!exit) {
 		SDL_Event sdlEvent;
-		while (SDL_PollEvent(&sdlEvent))
-		{
+		while (SDL_PollEvent(&sdlEvent)) {
 			ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
 
-			if (sdlEvent.type == SDL_QUIT)
-			{
+			if (sdlEvent.type == SDL_QUIT) {
 				exit = true;
 			}
 		}
@@ -36,7 +60,32 @@ int main(int argc, char* argv[])
 		ImGui_ImplSDL2_NewFrame();
 		ImGui::NewFrame();
 
-		ImGui::ShowDemoWindow();
+		ImGui::Begin("Demo");
+
+		const auto x = 1024 / 2;
+		const auto y = 768 / 2;
+
+		const auto mx = ImGui::GetMousePos().x - x;
+		const auto my = ImGui::GetMousePos().y - y;
+
+		const float length = std::sqrt(mx * mx + my * my);
+		float angle = ((int)(std::atan2(-my, -mx) * 180 / 3.14) + 180) / 360.0f;
+
+		Chart::Pie::Begin({x, y});
+		for (const auto& [radius, start, end, hue] : sliceDrawData | std::views::reverse) {
+			if (length <= radius && length >= radius - 40 && angle >= start && angle <= end) {
+				Chart::Pie::Color(ImColor::HSV(hue, 0.25f, 1.0f));
+			}
+			else {
+				Chart::Pie::Color(ImColor::HSV(hue, 1 - radius / 420, 1.0f));
+			}
+
+			Chart::Pie::Slice(radius, start, end);
+		}
+		Chart::Pie::End();
+
+		ImGui::End();
+		// ImGui::ShowDemoWindow();
 
 		ImGui::Render();
 
