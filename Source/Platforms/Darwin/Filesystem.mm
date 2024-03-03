3cdd67c3971753563f95dd1bbe0ce1d01588e59e
Sergei Belov <sergei-belov@outlook.com>
2024-03-04 01:07:28 +0200

    Still refactoring the filesystem namespace for better macOS compatibility.

--- Source/Platforms/Darwin/Filesystem.mm
+++ Source/Platforms/Darwin/Filesystem.mm
@@ -1,82 +1,43 @@
 // Copyright ❤️ 2023-2024, Sergei Belov
 
 #include <Filesystem.h>
-#include "fmt/xchar.h"
-
-#include <dirent.h>
-#include <sys/param.h>
-#include <sys/mount.h>
-
 #import <Foundation/Foundation.h>
 
 namespace Filesystem {
-	namespace Detail {
-		std::unordered_set<std::filesystem::path::string_type> Firmlinks = [](){
-			std::unordered_set<std::filesystem::path::string_type> links;
-
-			std::ifstream stream("/usr/share/firmlinks");
-			if (stream.is_open()) {
-				std::string line;
-				while (std::getline(stream, line)) {
-					if (const size_t delimiter = line.find('\t'); delimiter != std::string::npos) {
-						std::filesystem::path::string_type value = {line.begin(), line.begin() + delimiter};
-						links.emplace(value);
-					}
-				}
-			}
-
-			return links;
-		}();
-
-		bool IsSymlink(const std::filesystem::directory_iterator& iterator, std::error_code& error) {
-			auto& links = Detail::Firmlinks;
-			return iterator->is_symlink(error) || links.count(iterator->path().native());
-		}
-	}
-
 	std::vector<VolumeData> GetVolumesData() {
 		std::vector<VolumeData> result;
 		
-		auto* keys = [NSArray arrayWithObjects:
-		    NSURLVolumeNameKey,
-		    NSURLVolumeTotalCapacityKey,
-		    NSURLVolumeAvailableCapacityKey,
-		    nil
+		NSArray* propertyKeys = @[
+			NSURLVolumeNameKey,
+			NSURLVolumeTotalCapacityKey,
+			NSURLVolumeAvailableCapacityKey,
+			NSURLVolumeIsBrowsableKey
 		];
 		
-		auto* urls = [
-		    [NSFileManager defaultManager]
-		    mountedVolumeURLsIncludingResourceValuesForKeys:keys
-		    options:NSVolumeEnumerationSkipHiddenVolumes
+		NSArray* mountedVolumes = [[NSFileManager defaultManager]
+			mountedVolumeURLsIncludingResourceValuesForKeys:propertyKeys
+			options:0
 		];
 		
-		for(NSURL* url : urls) {
-		    auto* values = [url resourceValuesForKeys:keys error:NULL];
-		    
-		    auto& volume = result.emplace_back();
-		    volume.rootPath = [url.path UTF8String];
-		    volume.name = [values[NSURLVolumeNameKey] UTF8String];
-		    
-		    volume.bytesFree = [values[NSURLVolumeAvailableCapacityKey] unsignedLongLongValue];
-		    volume.bytesTotal = [values[NSURLVolumeTotalCapacityKey] unsignedLongLongValue];
+		for(NSURL* volumeURL in mountedVolumes) {
+			NSDictionary* propertyValues = [volumeURL resourceValuesForKeys:propertyKeys error:nil];
+			
+			if (![propertyValues[NSURLVolumeIsBrowsableKey] boolValue]) {
+				continue;
+			}
+			
+			VolumeData& volumeData = result.emplace_back();
+			volumeData.rootPath = [volumeURL.path cStringUsingEncoding:NSUTF8StringEncoding];
+			
+			volumeData.name = [propertyValues[NSURLVolumeNameKey] cStringUsingEncoding:NSUTF8StringEncoding];
+			
+			volumeData.bytesFree = [propertyValues[NSURLVolumeAvailableCapacityKey] unsignedLongLongValue];
+			volumeData.bytesTotal = [propertyValues[NSURLVolumeTotalCapacityKey] unsignedLongLongValue];
 		}
 		
 		return result;
 	}
-
-	std::pair<size_t, size_t> GetDriveSpace(std::string_view driveLetter) {
-		struct statfs stats;
-		size_t totalBytes = 0;
-		size_t freeBytes = 0;
-
-		if (statfs(("/Volumes/" + std::string(driveLetter)).c_str(), &stats) == 0) {
-			totalBytes = stats.f_blocks * stats.f_bsize;
-			freeBytes = stats.f_bfree * stats.f_bsize;
-		}
-
-		return std::make_pair(totalBytes, freeBytes);
-	}
-
+ 
 	void OpenSystemPath(const std::filesystem::path& value) {
 		system(fmt::format("open \"{}\"", value.native()).c_str());
 	}
@@ -86,34 +47,53 @@ namespace Filesystem {
 	}	
 
 	std::queue<NodeWrapper> EnumerateDirectory(Tree::Node<Entry>& node, std::atomic<size_t>& progress) {
-		std::error_code error;
 		std::queue<NodeWrapper> result;
-
-		auto iterator = std::filesystem::directory_iterator(node->path, error);
-		const auto end = std::filesystem::end(iterator);
-
-		const auto depth = node->depth + 1;
-
+		
 		size_t total = 0;
-		while (iterator != end) {
-			if (!error) {
-				if (iterator->is_symlink(error) || error) {
-					iterator.increment(error);
+		
+		const auto depth = node->depth + 1;
+		const char* nodePath = node->path.c_str();
+
+		NSURL* directoryURL = [NSURL fileURLWithPath:
+			[NSString stringWithCString:nodePath encoding:NSUTF8StringEncoding]
+		];
+		
+		NSArray* propertyKeys = @[
+			NSURLIsDirectoryKey,
+			NSURLIsSymbolicLinkKey,
+			NSURLIsVolumeKey,
+			NSURLVolumeIsBrowsableKey,
+			NSURLFileSizeKey
+		];
+		
+		NSArray* directoryContents = [[NSFileManager defaultManager]
+			contentsOfDirectoryAtURL:directoryURL
+			includingPropertiesForKeys:propertyKeys
+			options:0
+			error:nil
+		];
+		
+		for (NSURL* entryURL in directoryContents) {
+			NSDictionary* propertyValues = [entryURL resourceValuesForKeys:propertyKeys error:nil];
+			
+			if ([propertyValues[NSURLIsSymbolicLinkKey] boolValue]) {
+				continue;
+			}
+			else if ([propertyValues[NSURLIsVolumeKey] boolValue]) {
+				if ([propertyValues[NSURLVolumeIsBrowsableKey] boolValue]) {
 					continue;
-				}
-
-				auto& child = node.emplace(0, depth, iterator->path());
-
-				if (iterator->is_directory(error) && !error) {
-					result.emplace(std::ref(child));
-				}
-				else if (iterator->file_size(error) && !error) {
-					child->size = iterator->file_size(error);
-					total += child->size;
 				}
 			}
 
-			iterator.increment(error);
+			auto& child = node.emplace(0, depth, [[entryURL path] cStringUsingEncoding:NSUTF8StringEncoding]);
+			
+			if ([propertyValues[NSURLIsDirectoryKey] boolValue]) {
+				result.emplace(std::ref(child));
+			}
+			else {
+				child->size = [propertyValues[NSURLFileSizeKey] unsignedLongLongValue];
+				total += child->size;
+			}
 		}
 
 		progress += total;
