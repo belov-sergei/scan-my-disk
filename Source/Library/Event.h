ab643aced4475db1174861ea378c7095bdd7c0b7
Sergei Belov <sergei-belov@outlook.com>
2024-03-06 03:49:06 +0200

    Add event system implementation.

---
+++ Source/Library/Event.h
@@ -0,0 +1,38 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+#include "Storage.h"
+
+template <typename EventType>
+class Event final {
+public:
+	~Event() = delete;
+	
+	using ReceiverAddressType = void*;
+	using ReceiverCallbackType = std::function<void(EventType&)>;
+
+	using DataType = std::unordered_map<ReceiverAddressType, ReceiverCallbackType>;
+
+	template <typename ValueType>
+	using EventForwardingEnabled = std::enable_if_t<std::is_same_v<std::decay_t<ValueType>, EventType>, bool>;
+
+	template <typename ValueType, EventForwardingEnabled<ValueType>  = true>
+	static void Send(ValueType& event) {
+		Storage<Event>::Read([&](const DataType& storage) {
+			for (const auto& [receiver, callback] : storage) {
+				callback(event);
+			}
+		});
+	}
+
+	template <typename... ArgumentTypes>
+	static void Send(ArgumentTypes&&... arguments) {
+		Send<EventType>({std::forward<ArgumentTypes>(arguments)...});
+	}
+
+	static void Receive(ReceiverAddressType receiver, ReceiverCallbackType callback) {
+		Storage<Event>::Write([&](DataType& storage) {
+			storage[receiver] = std::move(callback);
+		});
+	}
+};
---
+++ Tests/Event/Test.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#include <gtest/gtest.h>
+#include <Event.h>
+
+struct TestEvent {
+	int counter = 0;
+};
+
+TEST(Event, Test) {
+	TestEvent myEvent;
+	myEvent.counter = 5;
+
+	Event<TestEvent>::Receive(nullptr, [](TestEvent& event) {
+		std::cout << event.counter << std::endl;
+	});
+
+	Event<TestEvent>::Send(myEvent);
+}
+
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
