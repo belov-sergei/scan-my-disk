95ef10e53700adbbde5f4b6b51eff2fc66b97c16
Sergei Belov <sergei-belov@outlook.com>
2024-03-06 03:39:52 +0200

    Add global storage class.

---
+++ Source/Library/Storage.h
@@ -0,0 +1,28 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+
+template <typename UserType>
+class Storage final {
+	using MutexType = std::shared_mutex;
+	using DataType = typename UserType::DataType;
+
+public:
+	~Storage() = delete;
+	
+	template <typename HandlerType>
+	static void Read(HandlerType& handler) {
+		std::shared_lock lock(mutex);
+		handler(storage);
+	}
+
+	template <typename HandlerType>
+	static void Write(HandlerType& handler) {
+		std::unique_lock lock(mutex);
+		handler(storage);
+	}
+
+private:
+	inline static MutexType mutex;
+	inline static DataType storage;
+};
