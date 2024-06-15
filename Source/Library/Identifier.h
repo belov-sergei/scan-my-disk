64ebbe90468af826113b2a4ab9920cac582727e0
Sergei Belov <sergei-belov@outlook.com>
2024-06-15 14:12:58 +0300

    Added a unique identifier class.

---
+++ Source/Library/Identifier.h
@@ -0,0 +1,34 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+
+template <typename IdentifiersGroup = void, typename InternalType = size_t>
+struct GenericIdentifier final {
+	struct Hash {
+		std::size_t operator()(const GenericIdentifier& identifier) const {
+			return std::hash<InternalType>()(identifier.value);
+		}
+	};
+
+	static GenericIdentifier Next() {
+		static InternalType value = 0;
+		return { value++ };
+	}
+
+	bool operator<(GenericIdentifier other) const {
+		return value < other.value;
+	}
+
+	bool operator==(GenericIdentifier other) const {
+		return value == other.value;
+	}
+
+private:
+	GenericIdentifier(InternalType value)
+	    : value(value) {}
+
+private:
+	InternalType value;
+};
+
+using Identifier = GenericIdentifier<>;
