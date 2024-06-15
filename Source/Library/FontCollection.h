1d7a4be1e01ff182664742c55c9ebe65d970397a
Sergei Belov <sergei-belov@outlook.com>
2024-06-15 14:23:14 +0300

    Added a font collection storage.

---
+++ Source/Library/FontCollection.h
@@ -0,0 +1,24 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+#include "Identifier.h"
+
+struct FontCollection final {
+	using DataType = FontCollection;
+	using BinaryFontDataType = std::vector<uint8_t>;
+
+	void CreateFont(Identifier fontId, BinaryFontDataType binaryFontData) {
+		collection[fontId] = std::move(binaryFontData);
+	}
+
+	void DeleteFont(Identifier fontId) {
+		collection.erase(fontId);
+	}
+
+	BinaryFontDataType& GetFont(Identifier fontId) {
+		return collection[fontId];
+	}
+
+private:
+	std::map<Identifier, BinaryFontDataType> collection;
+};
