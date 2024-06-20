b91d09c1091af25c49c25dcd1ec0f0e89706af45
Sergei Belov <sergei-belov@outlook.com>
2024-06-21 02:15:35 +0300

    Added forward iterator for working with memory regions.

---
+++ Source/Library/GenericForwardIterator.h
@@ -0,0 +1,47 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+
+// Represents a generic forward iterator. It is a base class for creating custom forward iterators.
+// It uses CRTP to provide basic iterator functionalities.
+//
+// The derived class must implement the following methods:
+// - bool Equal(const DerivedType&) const;
+// - DerivedType& Increment();
+// - ValueType& Dereference()
+template <typename DerivedType, typename ValueType>
+struct GenericForwardIterator {
+	using iterator_category = std::forward_iterator_tag;
+	using value_type = ValueType;
+	using difference_type = std::ptrdiff_t;
+	using pointer = ValueType*;
+	using reference = ValueType&;
+
+	[[nodiscard]] bool operator==(const DerivedType& other) const {
+		return static_cast<const DerivedType*>(this)->Equal(other);
+	}
+
+	[[nodiscard]] bool operator!=(const DerivedType& other) const {
+		return !operator==(other);
+	}
+
+	DerivedType& operator++() {
+		static_cast<DerivedType*>(this)->Increment();
+		return *static_cast<DerivedType*>(this);
+	}
+
+	DerivedType operator++(int) {
+		DerivedType value = *static_cast<DerivedType*>(this);
+		operator++();
+
+		return value;
+	}
+
+	[[nodiscard]] ValueType& operator*() {
+		return static_cast<DerivedType*>(this)->Dereference();
+	}
+
+	[[nodiscard]] ValueType* operator->() {
+		return &static_cast<DerivedType*>(this)->Dereference();
+	}
+};
---
+++ Source/Library/MemoryRegionIterator.h
@@ -0,0 +1,53 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+#include "GenericForwardIterator.h"
+
+// This class serves as an iterator for traversing a specific region of memory.
+// The iterator is parameterized by a type, which represents the type of value that the iterator points to.
+//
+// The iterator assumes that the memory region is a 2D region with a specified width and height,
+// and it allows traversal through this memory region in a row-major order.
+template <typename ValueType>
+struct MemoryRegionIterator final : GenericForwardIterator<MemoryRegionIterator<ValueType>, ValueType> {
+	MemoryRegionIterator() = default;
+
+	MemoryRegionIterator(int startX, int startY, int regionWidth, int regionHeight, int rowStride, ValueType* memoryStart)
+	    : regionWidth(regionWidth)
+	    , regionHeight(regionHeight)
+	    , rowStride(rowStride)
+	    , memoryStart(memoryStart + startY * rowStride + startX) {}
+
+	bool Equal(const MemoryRegionIterator& other) const {
+		if (other.memoryStart == nullptr) {
+			return currentX == regionWidth - 1 && currentY == regionHeight;
+		}
+
+		return &Dereference() == &other.Dereference();
+	}
+
+	MemoryRegionIterator& Increment() {
+		currentX = (currentX + 1) % regionWidth;
+		currentY += currentX == 0;
+
+		return *this;
+	}
+
+	ValueType& Dereference() {
+		return *(memoryStart + currentY * rowStride + currentX);
+	}
+
+	const ValueType& Dereference() const {
+		return *(memoryStart + currentY * rowStride + currentX);
+	}
+
+private:
+	int currentX = 0;
+	int currentY = 0;
+	int regionWidth = 0;
+	int regionHeight = 0;
+
+	// Distance between rows in the memory.
+	int rowStride = 0;
+	ValueType* memoryStart = nullptr;
+};
