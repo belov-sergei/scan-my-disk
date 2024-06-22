01dd7b87ac1f1caf49d3d99fdf4e89201cbebf69
Sergei Belov <sergei-belov@outlook.com>
2024-06-23 00:06:07 +0300

    Using built-in texts instead of external Translations.xml.

--- Source/Library/Components/LocalizationComponent.h
+++ Source/Library/Components/LocalizationComponent.h
@@ -2,31 +2,16 @@
 
 #pragma once
 #include "Application.h"
-#include "Localization.h"
 #include "Settings.h"
-
-#include <pugixml.hpp>
 
 struct LocalizationComponent final {
 	LocalizationComponent() {
 		Event<Application::Launch>::Receive(this, [](const auto&) {
-			pugi::xml_document xml;
-			xml.load_file("Translations.xml");
-
-			const auto root = xml.document_element();
-
-			for (const auto language : root) {
-				Localization::Language(language.name());
-
-				for (const auto text : language) {
-					const auto textId = text.attribute("Id").as_string();
-					const auto value = text.attribute("Value").as_string();
-
-					Localization::Text::Add(textId, value);
-				}
-			}
-
+			LoadText();
 			Localization::Language(Settings<User>::Language);
 		});
 	}
+
+private:
+	static void LoadText();
 };
