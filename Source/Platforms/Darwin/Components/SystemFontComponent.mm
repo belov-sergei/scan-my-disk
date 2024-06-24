5e2d60d23c4bc261180ef055c2a43a3f10801b88
Sergei Belov <sergei-belov@outlook.com>
2024-06-25 00:48:51 +0300

    Loading system font on macOS.

--- Source/Platforms/Darwin/Components/SystemFontComponent.mm
+++ Source/Platforms/Darwin/Components/SystemFontComponent.mm
@@ -2,7 +2,18 @@
 
 #include "Components/SystemFontComponent.h"
 
+#import <Cocoa/Cocoa.h>
+
 FontCollection::BinaryFontDataType SystemFontComponent::GetBinaryFontData() {
-	FontCollection::BinaryFontDataType binaryFontData;
-	return binaryFontData;
+	NSFont* systemFont = [NSFont systemFontOfSize:18];
+	
+	CTFontRef ctFont = (__bridge CTFontRef)systemFont;
+	CFTypeRef urlAttribute = CTFontCopyAttribute(ctFont, kCTFontURLAttribute);
+	
+	NSData* contentData = [NSData dataWithContentsOfURL:(__bridge NSURL *)urlAttribute];
+	CFRelease(urlAttribute);
+	
+	const uint8_t* binaryData = (const uint8_t*)[contentData bytes];
+
+	return { binaryData, binaryData + [contentData length] };
 }
