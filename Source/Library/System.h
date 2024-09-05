1d41cef78163298209c05b8f9951b3da14934223
Sergei Belov <sergei-belov@outlook.com>
2024-09-05 20:50:34 +0300

    Added functions to get the memory used by the process.

---
+++ Source/Library/System.h
@@ -0,0 +1,8 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#pragma once
+
+namespace System {
+	int64_t GetVirtualMemoryUsed();
+	int64_t GetPhysicalMemoryUsed();
+} // namespace System
---
+++ Source/Platforms/Windows/System.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#include "System.h"
+
+#include <Windows.h>
+// clang-format off
+#include <psapi.h>
+// clang-format on
+
+namespace System {
+	int64_t GetVirtualMemoryUsed() {
+		PROCESS_MEMORY_COUNTERS_EX processMemoryCounters;
+		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&processMemoryCounters, sizeof(processMemoryCounters));
+
+		return processMemoryCounters.PrivateUsage / 1024;
+	}
+
+	int64_t GetPhysicalMemoryUsed() {
+		PROCESS_MEMORY_COUNTERS_EX processMemoryCounters;
+		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&processMemoryCounters, sizeof(processMemoryCounters));
+
+		return processMemoryCounters.WorkingSetSize  / 1024;
+	}
+} // namespace System
