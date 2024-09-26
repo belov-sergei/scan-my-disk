9c5185eb9e91c221525895d370e79ee9418ba3e4
Sergei Belov <sergei-belov@outlook.com>
2024-09-27 01:20:38 +0300

    Added system memory usage tracking for macOS.

---
+++ Source/Platforms/Darwin/System.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright ❤️ 2023-2024, Sergei Belov
+
+#include "System.h"
+#include <mach/mach.h>
+
+namespace System {
+	int64_t GetVirtualMemoryUsed() {
+		task_basic_info_data_t taskInfo;
+		mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
+
+		task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&taskInfo, &infoCount);
+		return taskInfo.virtual_size;
+	}
+
+	int64_t GetPhysicalMemoryUsed() {
+		task_basic_info_data_t taskInfo;
+		mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
+
+		task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&taskInfo, &infoCount);
+		return taskInfo.resident_size;
+	}
+} // namespace System
